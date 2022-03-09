9375541cc19afb9d3ff44a37b81d9ddb2aa49a92
Paul Applegate <paul@theapplegates.com>
2022-03-09 16:33:29 +0000

    test

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,39 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
+import '@docsearch/css'
 
 import NewsletterForm from '@/components/NewsletterForm'
+
+import { DocSearch } from '@docsearch/react';
+import Head from 'next/head';
+
+export const SEARCH_API_KEY = process.env.NEXT_PUBLIC_ALGOLIA_KEY;
+export const SEARCH_APP_ID = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID;
+
+export function Search() {
+  if (!SEARCH_APP_ID || !SEARCH_API_KEY) {
+    return null;
+  }
+
+  return (
+    <>
+      <Head>
+        <link
+          rel="preconnect"
+          href={`https://${SEARCH_APP_ID}-dsn.algolia.net`}
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <DocSearch
+        apiKey={SEARCH_API_KEY}
+        indexName={'docs'}
+        appId={SEARCH_APP_ID}
+      />
+    </>
+  );
+}
+
 
 const MAX_DISPLAY = 5
 
